44b29eb25d849b8f96b0216b995098f929d25aae
sa46lll <9812smh@naver.com>
2023-01-28 01:20:53 +0900

    feat: gptService 생성

---
+++ src/main/java/com/sparcs/teamf/gpt/GptMessageGenerator.java
@@ -0,0 +1,24 @@
+package com.sparcs.teamf.gpt;
+
+import static com.sparcs.teamf.gpt.constant.GptMessageConstants.FOR_ANSWER_FORMAT;
+import static com.sparcs.teamf.gpt.constant.GptMessageConstants.FOR_BASIC_QUESTION_FORMAT;
+import static com.sparcs.teamf.gpt.constant.GptMessageConstants.FOR_TAIL_QUESTION_FORMAT;
+
+import com.sparcs.teamf.domain.midcategory.MidCategory;
+import com.sparcs.teamf.domain.question.Question;
+
+public class GptMessageGenerator {
+
+    public String generateForBasicQuestion(MidCategory midCategory) {
+        return String.format(FOR_BASIC_QUESTION_FORMAT, midCategory.getName());
+    }
+
+    public String generateForTailQuestion(Question question) {
+        return String.format(FOR_TAIL_QUESTION_FORMAT, question.getMidCategory().getName(), question.getQuestion());
+    }
+
+    public String generateForAnswer(Question question) {
+        return String.format(FOR_ANSWER_FORMAT, question.getQuestion());
+    }
+}
+
