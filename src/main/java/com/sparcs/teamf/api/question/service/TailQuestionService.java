3c09b7b6831e27cd459d736e87d030286facb956
sa46lll <9812smh@naver.com>
2023-01-28 14:30:44 +0900

    feat: 꼬리 질문

--- src/main/java/com/sparcs/teamf/api/question/service/TailQuestionService.java
+++ src/main/java/com/sparcs/teamf/api/question/service/TailQuestionService.java
@@ -1,5 +1,11 @@
 package com.sparcs.teamf.api.question.service;
 
+import com.sparcs.teamf.api.answer.exception.QuestionNotFoundException;
+import com.sparcs.teamf.api.question.dto.TailQuestionResponse;
+import com.sparcs.teamf.common.util.Repeat;
+import com.sparcs.teamf.domain.question.Question;
+import com.sparcs.teamf.domain.question.QuestionRepository;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -7,4 +13,26 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class TailQuestionService {
 
+    private final QuestionRepository questionRepository;
+
+    public TailQuestionResponse getTailQuestion(long questionId) {
+        Question question = findQuestionById(questionId).orElseThrow(QuestionNotFoundException::new);
+
+        if (question.getAnswer().isBlank()) {
+            Repeat.repeat(() -> findQuestionById(questionId), )
+        }
+
+        if (target)
+    }
+
+    private Optional<Question> findQuestionById(long questionId) {
+        return questionRepository.findById(questionId);
+    }
+
+    private boolean needToRepeat(Optional<Question> question) {
+        if (question.isEmpty()) {
+            return true;
+        }
+        return question.get().getAnswer() == null;
+    }
 }
